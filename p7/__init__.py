e5849744a3cce645be6ea92cea213982060ed1ce
Rashed <rashed@ishraak.com>
2020-09-27 16:47:00 +0600

    log file name changed

--- p7/__init__.py
+++ p7/__init__.py
@@ -1,7 +1,7 @@
 import logging
 
 logging.basicConfig(
-    filename=f'jobxprss.log',
+    filename=f'jobxprss-api.log',
     format='%(levelname)s [%(asctime)s]: %(message)s \nat %(funcName)s() in %(filename)s line %(lineno)s ',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO
