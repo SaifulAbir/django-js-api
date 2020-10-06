5bec5a2634833bf59972d369c488d9f59e20a184
saiful <saiful@ishraak.com>
2020-10-06 12:48:16 +0600

    professional list api modified

--- pro/api_ml.py
+++ pro/api_ml.py
@@ -8,16 +8,20 @@ from pro.serializers import ProfessionalSerializerAdmin
 
 
 class MlProfessionalList(ListAPIView):
-    # required_privilege = 'pro.view_professional'
-    permission_classes = []
+    required_privilege = 'pro.view_professional'
+    permission_classes = [StaffPermission]
     serializer_class = ProfessionalSerializerAdmin
 
     def get_queryset(self):
+        request = self.request
+        job_search_preference = request.GET.get('job_search_preference')
         queryset = Professional.objects.all(
         ).prefetch_related(
             Prefetch('skills',
-                     queryset=ProfessionalSkill.objects.filter(is_archived=False).order_by('skill_name')),
+                     queryset=ProfessionalSkill.objects.filter(is_top_skill=True, is_archived=False).order_by('skill_name')),
             Prefetch('pro_location_preferences',
                      queryset=ProfessionalLocationPreference.objects.filter(is_archived=False)),
         )
+        if job_search_preference:
+            queryset = queryset.filter(job_search_preference=job_search_preference)
         return queryset
