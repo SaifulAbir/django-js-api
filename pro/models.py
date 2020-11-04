b0c0a7ef82389f842b7f74fdd8817a2d895c9e9c
shoab <shoab@ishraak.com>
2020-11-04 17:16:16 +0600

    professional model modified for mobile verification

--- pro/models.py
+++ pro/models.py
@@ -142,6 +142,9 @@ class Professional(P7Model):
                                              choices=strings_pro.SEARCH_PREFERENCE_OPTION,
                                              default=strings_pro.DEFAULT_SEARCH_PREFERENCE_OPTION)
     notification_on = models.BooleanField(default=False)
+    is_mobile_verified = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False)
+    mobile_verification_code = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.full_name
