7b235ac3160d61d9b7a12c31bcdbbd7f832b1b0f
mahadi <mahadi@ishraak.com>
2020-11-10 17:37:14 +0600

    v1.0.45

--- p7/settings_dev.py
+++ p7/settings_dev.py
@@ -205,7 +205,7 @@ MEDIA_URL = '/media/'
 
 SESSION_COOKIE_AGE = 6000
 SITE_URL = 'https://jobxprss.com'
-APP_VERSION_NUMBER = 'v1.0.44'
+APP_VERSION_NUMBER = 'v1.0.45'
 ## ToDo Shohag news_dev for testing news for production
 FIREBASE_CLOUD_MESSAGING_TOPIC = 'news'
 
