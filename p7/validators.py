c81a57b895f58c6effb556356f3bfcafc47fed22
shoab <shoab@ishraak.com>
2020-12-12 19:07:44 +0600

    validatir .py update mobile validation

--- p7/validators.py
+++ p7/validators.py
@@ -35,7 +35,7 @@ def check_valid_password(value):
         raise ValidationError(DEFAULT_INVALID_PASSWORD_ERROR)
 
 def check_valid_phone_number(value):
-    if not re.findall('^(?:\+?88)?01[15-9]\d{8}$', value):
+    if not re.findall('^01[13-9]\d{8}$', value):
         raise ValidationError(DEFAULT_INVALID_PHONE_NUMBER_ERROR)
 
 class ResetPasswordValidator(object):
