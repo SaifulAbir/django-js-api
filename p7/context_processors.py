03ce545ceba21efbe70420a0508d00e2378ae203
Rashed <rashed@ishraak.com>
2021-01-04 16:08:15 +0600

    working on PDF generate

--- p7/context_processors.py
+++ p7/context_processors.py
@@ -2,3 +2,9 @@ from django.conf import settings
 
 def footer_context(request):
     return {'APP_VERSION_NUMBER': settings.APP_VERSION_NUMBER}
+
+def media_context(request):
+    return {'MEDIA_BUCKET_URL_PREFIX': settings.MEDIA_BUCKET_URL_PREFIX}
+
+def static_context(request):
+    return {'STATIC_CLOUD_FONT_URL_PREFIX': settings.STATIC_URL}
