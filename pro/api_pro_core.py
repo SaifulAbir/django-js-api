631dceef782b8835e4b8dd09becf542b28806ff7
Rashed <rashed@ishraak.com>
2021-01-05 09:40:46 +0600

    Unable apply issue solved

--- pro/api_pro_core.py
+++ pro/api_pro_core.py
@@ -550,7 +550,8 @@ class DownloadApplicantResumeAPIView(generics.RetrieveAPIView):
             Prefetch('references',
                      queryset=Reference.objects.filter(is_archived=False).order_by('created_at'))
         )
-        html = self.template.render({'data': queryset, 'SITE_URL': settings.SITE_URL})
+        html = self.template.render({'data': queryset, 'SITE_URL': settings.SITE_URL,'STATIC_URL': settings.STATIC_URL,
+                                     'MEDIA_BUCKET_URL_PREFIX': settings.MEDIA_BUCKET_URL_PREFIX})
         options = {
             'page-size': "A4",
             'encoding': "UTF-8",
