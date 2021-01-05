f8fa0c5a6a4900014c7cb32954ffded3bd5c7260
Rashed <rashed@ishraak.com>
2021-01-05 10:01:16 +0600

    job apply static url and media url added.

--- job/api_misc.py
+++ job/api_misc.py
@@ -241,7 +241,7 @@ class JobApplicationAPI(APIView):
                      queryset=Reference.objects.filter(is_archived=False).order_by('created_at'))
         )
         html = self.template.render({'data': queryset, 'SITE_URL': settings.SITE_URL,
-                                     'SITE_URL': settings.SITE_URL, 'SITE_URL': settings.SITE_URL})
+                                     'MEDIA_BUCKET_URL_PREFIX': settings.MEDIA_BUCKET_URL_PREFIX, 'STATIC_URL': settings.STATIC_URL})
         options = {
             'page-size': "A4",
             'encoding': "UTF-8",
